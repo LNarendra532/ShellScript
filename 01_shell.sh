f97add54e8049a993716210a5b69211eaf3551c7
Naren <lnaren532@gmail.com>
2025-05-15 12:24:01 +0530

    shell-practice-commit

---
+++ 01_shell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+name1=Ramesh
+name2=$name2
+
+echo " $name1:HI  how  are you $name2 "
+echo "$name2: , Iam doing godd $name1 , How about you"
+echo "$name1 :Iam also doing good , what's app "
