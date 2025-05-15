9705f84a83fec1d5d1b6633225bcb3a7e4b132ef
Naren <lnaren532@gmail.com>
2025-05-15 12:46:57 +0530

    shell-practice-commit

--- array.sh
+++ array.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
 
-#Moives=("court" "Pushpa2" "Bimbisara" "salar")
+# #Moives=("court" "Pushpa2" "Bimbisara" "salar")
+# Movies=("Court" "HIT3" "PUSHPA2" "Thandel")
+# #There should be no gap , while assiging the values i.e = a=1
+# #Above one array we can using index values Movies(0,1,2,3,4,...)
+
+# echo "First movie ${Movies[0]}"
+# echo "THird Move ${Movies[2]}"
+# echo "Acces all Mobvues ${Movies[@]}"
+
+# Time_stamp = $(date)
+# echo "scipt excuted at $Time_stamp"
+
+!/bin/bash
+
 Movies=("Court" "HIT3" "PUSHPA2" "Thandel")
-#There should be no gap , while assiging the values i.e = a=1
-#Above one array we can using index values Movies(0,1,2,3,4,...)
 
-echo "First movie ${Movies[0]}"
-echo "THird Move ${Movies[2]}"
-echo "Acces all Mobvues ${Movies[@]}"
+echo "First movie: ${Movies[0]}"
+echo "All movies: ${Movies[@]}"
+echo "Total movies: ${#Movies[@]}"
 
-Time_stamp = $(date)
-echo "scipt excuted at $Time_stamp"
+for movie in "${Movies[@]}"; do
+    echo "Movie: $movie"
+done
