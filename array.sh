c1dc8063152597fec61f7e2704baac16b0976f52
Naren <lnaren532@gmail.com>
2025-05-15 12:35:38 +0530

    shell-practice-commit

--- array.sh
+++ array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-Moives = ("court" , "Pushpa2","Bimbi sara" ,"salar")
+Moives = ("court" "Pushpa2" "Bimbisara" "salar")
 #Above one array we can using index values Movies(0,1,2,3,4,...)
 echo "First movie $Movies[0]"
 echo "THird Move $Movies[2]"
