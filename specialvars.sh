9c16b60465f1142e1bb63b93ae3a36a7ddb06f76
Naren <lnaren532@gmail.com>
2025-05-16 12:17:40 +0530

    shell script

--- specialvars.sh
+++ specialvars.sh
@@ -11,3 +11,16 @@ sleep 10 &
 echo "8.PID of last command in background: $!"
 
 # $@, $$ -PID , $# total , $
+
+# output
+# -------
+# 13.220.137.245 | 172.31.86.244 | t2.micro | https://github.com/LNarendra532/ShellScript.git
+# [ ec2-user@ip-172-31-86-244 ~/ShellScript ]$ sh specialvars.sh "abcdefgh anbsdf" "werwqr"
+# 1All variables passed to the script: abcdefgh anbsdf werwqr
+# 2Number of variables: 2
+# 3Script name: specialvars.sh
+# 4Current Directory: /home/ec2-user/ShellScript
+# 5User running this script: ec2-user
+# 6Home directory of user: /home/ec2-user
+# 7PID of the script: 1771
+# 8.PID of last command in background: 1772
