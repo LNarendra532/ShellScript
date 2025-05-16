fc39fefa3a54db9bec6fc608b4cb6eaa86537d1f
Naren <lnaren532@gmail.com>
2025-05-16 13:18:16 +0530

    shell script

--- install_commands.sh
+++ install_commands.sh
@@ -19,3 +19,16 @@ else
     echo "dnf is not installing... FAILURE"
     exit 1
 fi
+
+# output 
+__________
+
+# [ ec2-user@ip-172-31-86-244 ~/ShellScript ]$ sh install_commands.sh
+# Not a root User 1001 ec2-user
+# Error: This command has to be run with superuser privileges (under the root user on most systems).
+# dnf is not installing... FAILURE
+
+# 13.220.137.245 | 172.31.86.244 | t2.micro | https://github.com/LNarendra532/ShellScript.git
+# [ ec2-user@ip-172-31-86-244 ~/ShellScript ]$ echo  "$?"
+# 1
+
