ddfc411b25fa70b0df1651187e7af3efd180f565
Naren <lnaren532@gmail.com>
2025-05-16 13:04:05 +0530

    shell script

--- install_commands.sh
+++ install_commands.sh
@@ -1,11 +1,11 @@
 #!/bin/bash/
 
-USER = $(id -u)
-echo "user is $USER"
+USERID=$(id -u)
+#echo "user is $USER"
 
-if [ $USER -ne 0]
+if [ $USERID -ne 0]
 then
     echo "Not a root User"
 else
-    echo "your running with $USER"
+    echo "your running with $USERID"
 fi
