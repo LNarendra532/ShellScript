e1b9c45684c4736afc2c4df739c4ebc14fb909bf
Naren <lnaren532@gmail.com>
2025-05-16 12:00:35 +0530

    shell script

--- arr_access_values.sh
+++ arr_access_values.sh
@@ -2,10 +2,8 @@
 
 
 Moives=("court" "Pushpa2" "Bimbisara" "salar")
-
 #There should be no gap , while assiging the values i.e = a=1
 #Above one array we can using index values Movies(0,1,2,3,4,...)
-
 echo "First movie: ${Moives[0]}"
 echo "All movies: ${Moives[@]}"
 echo "Third Index"
