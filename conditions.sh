11c60c06ea1f96c57f1acc3766c85e530701b745
Naren <lnaren532@gmail.com>
2025-05-16 12:44:01 +0530

    shell script

--- conditions.sh
+++ conditions.sh
@@ -8,3 +8,18 @@ then
 else
     echo "$Number not less than 10"
 fi
+
+# output
+# ------
+
+# 13.220.137.245 | 172.31.86.244 | t2.micro | https://github.com/LNarendra532/ShellScript.git
+# [ ec2-user@ip-172-31-86-244 ~/ShellScript ]$ sh conditions.sh 10
+# 10 not less than 10
+
+# 13.220.137.245 | 172.31.86.244 | t2.micro | https://github.com/LNarendra532/ShellScript.git
+# [ ec2-user@ip-172-31-86-244 ~/ShellScript ]$ sh conditions.sh 5
+# 5 less than than 10
+
+# 13.220.137.245 | 172.31.86.244 | t2.micro | https://github.com/LNarendra532/ShellScript.git
+# [ ec2-user@ip-172-31-86-244 ~/ShellScript ]$ sh conditions.sh 5 20
+# 5 less than than 10
