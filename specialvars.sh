d5cbb363244d7fd40b5501e4d9cb93d414e895b3
Naren <lnaren532@gmail.com>
2025-05-16 12:16:25 +0530

    shell script

--- specialvars.sh
+++ specialvars.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "1.All variables passed to the script: $@"
-echo "2.Number of variables: $#"
-echo "3.Script name: $0"
-echo "4.Current Directory: $PWD"
-echo "5.User running this script: $USER"
-echo "6.Home directory of user: $HOME"
-echo "7.PID of the script: $$"
+echo "1All variables passed to the script: $@"
+echo "2Number of variables: $#"
+echo "3Script name: $0"
+echo "4Current Directory: $PWD"
+echo "5User running this script: $USER"
+echo "6Home directory of user: $HOME"
+echo "7PID of the script: $$"
 sleep 10 &
 echo "8.PID of last command in background: $!"
 
