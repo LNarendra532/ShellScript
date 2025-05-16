4234ab3b7f5532ae9219a145c6e8aaceaa26947b
Naren <lnaren532@gmail.com>
2025-05-16 12:23:11 +0530

    shell script

--- specialvars.sh
+++ specialvars.sh
@@ -15,14 +15,21 @@ echo "9 wihch gives the bakground running id , ran by last commands: $!"
 # $@, $$ -PID , $# total , $USER , $$ $PWD $$ $USER $HOME 
 
 # output
-# -------
-# 13.220.137.245 | 172.31.86.244 | t2.micro | https://github.com/LNarendra532/ShellScript.git
+
 # [ ec2-user@ip-172-31-86-244 ~/ShellScript ]$ sh specialvars.sh "abcdefgh anbsdf" "werwqr"
-# 1All variables passed to the script: abcdefgh anbsdf werwqr
-# 2Number of variables: 2
-# 3Script name: specialvars.sh
-# 4Current Directory: /home/ec2-user/ShellScript
-# 5User running this script: ec2-user
-# 6Home directory of user: /home/ec2-user
-# 7PID of the script: 1771
-# 8.PID of last command in background: 1772
+# 1 All variables passed to the script: abcdefgh anbsdf werwqr
+# 2 Number of variables: 2
+# 3 Script name: specialvars.sh
+# 4 Current Directory: /home/ec2-user/ShellScript
+# 5 which User running this script: ec2-user
+# 6 Home directory of user: /home/ec2-user
+# 7 PID of the script: 1837
+# 8 PID of last command in background: 1838
+# 9 wihch gives the bakground running id , ran by last commands: 1839
+
+# 13.220.137.245 | 172.31.86.244 | t2.micro | https://github.com/LNarendra532/ShellScript.git
+# [ ec2-user@ip-172-31-86-244 ~/ShellScript ]$ ps -ef | grep sleep
+# ec2-user    1832       1  0 06:51 pts/0    00:00:00 sleep 10
+# ec2-user    1838       1  0 06:51 pts/0    00:00:00 sleep 10
+# ec2-user    1839       1  0 06:51 pts/0    00:00:00 sleep 5
+# ec2-user    1844    1323  0 06:51 pts/0    00:00:00 grep --color=auto sleep
