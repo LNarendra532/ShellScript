e2db8f5e4a90b4e02293afdabec991e2c0867392
Naren <lnaren532@gmail.com>
2025-05-16 13:44:16 +0530

    shell script

--- funtions.sh
+++ funtions.sh
@@ -9,3 +9,27 @@ then
 else
     echo "success : running with root access"
 fi
+## output : success : running with root access
+
+# below program is for installing the 'python3 mysql and nginx'
+# passing the 3 package to the funtions
+
+VALIDATE(){
+    if [ $1 -eq 0 ]        
+    then
+        echo "success: $2  installing "
+    else
+        echo "Failure: $2 is not installing"
+}
+
+# we are the pkg is laredy insatlled are not,
+dnf list installed python3
+
+if [ $? -ne 0 ]
+then
+    echo "python3 not installed .. going to install.."
+    sudo dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo "python has been Installed alredy"
+
