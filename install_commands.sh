7ad329a9ec2d1bdca431d7ab1389e2f13b0f7982
Naren <lnaren532@gmail.com>
2025-05-16 13:17:07 +0530

    shell script

--- install_commands.sh
+++ install_commands.sh
@@ -10,12 +10,12 @@ else
     echo "your running with ROOT"
 fi
 
-dnf install mysql -y
+dnf install nginx -y
 
 if [ $? -eq 0 ]
 then 
     echo "dnf is installing...SUCCESS"
 else
     echo "dnf is not installing... FAILURE"
-    #exit 1
+    exit 1
 fi
