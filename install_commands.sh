3ca00d16157d1c6a324bd7da3c1f4ceaad51198d
Naren <lnaren532@gmail.com>
2025-05-16 13:13:49 +0530

    shell script

--- install_commands.sh
+++ install_commands.sh
@@ -7,14 +7,15 @@ if [ $USERID -ne 0 ]
 then
     echo "Not a root User $USERID $USER"
 else
-    echo "your running with "
+    echo "your running with ROOT"
 fi
 
 dnf install mysql -y
 
 if [ $? -eq 0 ]
 then 
-    echo "dnf is installing...SUCCESS""
+    echo "dnf is installing...SUCCESS"
 else
-    echo "dnf is not installing.. FAILURE"
+    echo "dnf is not installing... FAILURE"
+    exit 1
 fi
