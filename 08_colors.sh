f2e273deb93965fba24e57a9e839eb0edd9ee19a
Naren <lnaren532@gmail.com>
2025-05-19 15:20:46 +0530

    shell script

---
+++ 08_colors.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "\e[31m Hello Colors \e[0m"
+
+echo "Hello No Colors
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo -e "$R Hello Colors $N"
+echo -e "$G Hello Colors $N"
+echo -e "$Y Hello Colors $N"
